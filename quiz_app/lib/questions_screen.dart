6ac7b5c43c10bf4babb5f1868469926ae6206853
Omar Masoud <omarjaffar3@gmail.com>
2023-08-05 17:46:45 +0300

    Add answers shuffle functionality

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/question_buttons.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'dart:math';
+
+Object randomizer = Random();
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,30 +17,33 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Center(
+    var order = [0, 1, 2, 3];
+    order.shuffle();
+    return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
-            "the Question",
-            style: TextStyle(fontSize: 30),
+            questions[0].text,
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 30, color: Colors.white),
           ),
-          SizedBox(
+          const SizedBox(
             height: 30,
           ),
-          QuestionButton(answer: "answer 1"),
-          SizedBox(
+          QuestionButton(answer: questions[0].answers[order[0]]),
+          const SizedBox(
             height: 10,
           ),
-          QuestionButton(answer: "answer 2"),
-          SizedBox(
+          QuestionButton(answer: questions[0].answers[order[1]]),
+          const SizedBox(
             height: 10,
           ),
-          QuestionButton(answer: "answer 3"),
-          SizedBox(
+          QuestionButton(answer: questions[0].answers[order[2]]),
+          const SizedBox(
             height: 10,
           ),
-          QuestionButton(answer: "answer 4"),
+          QuestionButton(answer: questions[0].answers[order[3]]),
         ],
       ),
     );
